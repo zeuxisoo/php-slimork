7e43e56807f1fe86d54be77b9f4c01a217818de2
Zeuxis Lo <seekstudio@gmail.com>
2012-02-15 00:59:40 +0800

    first prototype

---
+++ index.php
@@ -0,0 +1,86 @@
+<?php
+error_reporting(E_ALL);
+define('IN_APPS', true);
+
+// Define constant
+define('WWW_ROOT', dirname(__FILE__));
+define('APP_ROOT', dirname(__FILE__).'/app');
+define('CACHE_ROOT', dirname(__FILE__).'/cache');
+define('CONFIG_ROOT', dirname(__FILE__).'/config');
+define('LIB_ROOT', dirname(__FILE__).'/lib');
+define('LOG_ROOT', dirname(__FILE__).'/log');
+define('VENDOR_ROOT', dirname(__FILE__).'/vendor');
+define('ROUTERS_ROOT', APP_ROOT.'/routes');
+define('MODELS_ROOT', APP_ROOT.'/models');
+define('VIEWS_ROOT', APP_ROOT.'/views');
+
+// Initial global variable
+$config = array();
+
+// Import config file
+require_once CONFIG_ROOT.'/common.php';
+require_once CONFIG_ROOT.'/database.php';
+
+// Import vendor files
+require_once VENDOR_ROOT.'/Slim/Slim.php';
+require_once VENDOR_ROOT.'/Slim-Extras/Views/HaangaView.php';
+require_once VENDOR_ROOT.'/idiorm.php';
+require_once VENDOR_ROOT.'/pairs.php';
+
+// Define auto load workflow
+spl_autoload_register("auto_load");
+
+function auto_load($class_name) {
+	global $config;
+
+	foreach(array('lib') as $folder) {
+		$file_path = WWW_ROOT.'/'.$folder.'/'.strtolower($class_name).'.php';
+
+		if (file_exists($file_path) === true && is_file($file_path) === true) {
+			require_once $file_path;
+		}
+	}
+}
+
+// Configure database
+ORM::configure($config['database']['dsn']);
+
+if (empty($config['database']['username']) === false) {
+	ORM::configure('username', $config['database']['username']);
+	ORM::configure('password', $config['database']['password']);
+}
+
+if (substr(strtolower($config['database']['dsn']), 0, 5) === 'mysql') {
+	ORM::configure('driver_options', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+}
+
+// Initial slim framework
+$app = new Slim(array(
+	'mode'               => $config['common']['application_mode'],
+	'view'               => new HaangaView(VENDOR_ROOT.'/Haanga', VIEWS_ROOT, CACHE_ROOT.'/views'),
+	'templates.path'     => VIEWS_ROOT,
+	'debug'              => $config['common']['enable_debug'],
+	'log.enable'         => $config['common']['enable_log'],
+	'log.path'           => LOG_ROOT,
+	'cookies.lifetime'   => $config['common']['cookies_life_time'],
+	'cookies.secret_key' => $config['common']['cookies_secret_key'],
+));
+
+// Auto import all routers, models, views file
+$directories = array(MODELS_ROOT, VIEWS_ROOT, ROUTERS_ROOT);
+
+while (sizeof($directories)) {
+	$directory = array_pop($directories);
+
+	foreach(glob($directory."/*") as $file_path) {
+		if (is_dir($file_path) === true) {
+			array_push($directories, $file_path);
+		}elseif (is_file($file_path) === true && preg_match("/\.(php|inc)$/", $file_path) == true) {
+			require_once $file_path;
+		}
+	}
+}
+
+// Boot application
+$app->run();
+?>
