f80002eea78145fc86802a443de4ef136f3e77e6
Zeuxis Lo <seekstudio@gmail.com>
2016-04-05 15:34:44 +0800

    add type to arguments in ServiceProvider constructor

--- app/Constracts/ServiceProvider.php
+++ app/Constracts/ServiceProvider.php
@@ -1,11 +1,13 @@
 <?php
 namespace App\Constracts;
 
+use Interop\Container\ContainerInterface;
+
 abstract class ServiceProvider {
 
     protected $container;
 
-    public function __construct($container) {
+    public function __construct(ContainerInterface $container) {
         $this->container = $container;
     }
 
