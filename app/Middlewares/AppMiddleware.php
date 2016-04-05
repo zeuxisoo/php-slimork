a219cfa2e163cf172e73cb36731360b320cbf3d6
Zeuxis Lo <seekstudio@gmail.com>
2016-04-05 18:09:36 +0800

    update default app middleware

--- app/Middlewares/AppMiddleware.php
+++ app/Middlewares/AppMiddleware.php
@@ -1,11 +1,13 @@
 <?php
 namespace App\Middlewares;
 
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
 use App\Contracts\Middleware;
 
 class AppMiddleware extends Middleware {
 
-    public function __invoke($request, $response, $next) {
+    public function __invoke(Request $request, Response $response, $next) {
         return $next($request, $response);
     }
 
