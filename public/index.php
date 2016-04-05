5ad83a021c115137001f276afd0237256c75b34c
Zeuxis Lo <seekstudio@gmail.com>
2016-04-05 13:33:45 +0800

    add slim twig view package

--- public/index.php
+++ public/index.php
@@ -1,12 +1,58 @@
 <?php
-require_once dirname(__DIR__).'/vendor/autoload.php';
+error_reporting(E_ALL);
+session_start();
 
-$app = new Slim\App();
+// Define constant
+define('BASE_ROOT', dirname(__DIR__));
+define('VENDOR_ROOT', BASE_ROOT.'/vendor');
+define('CONFIG_ROOT', BASE_ROOT.'/config');
+define('RESOURCE_ROOT', BASE_ROOT.'/resource');
+define('STORAGE_ROOT', BASE_ROOT.'/storage');
+
+// Composer auto loader
+require_once VENDOR_ROOT.'/autoload.php';
+
+// Application configs
+$config = [
+    'app'  => require CONFIG_ROOT.'/app.php',
+    'slim' => require CONFIG_ROOT.'/slim.php',
+];
+
+// Base configs
+date_default_timezone_set($config['app']['timezone']);
+
+// Import class
+use Slim\App;
+use Slim\Views\Twig;
+use Slim\Views\TwigExtension;
+
+// Setup slim
+$app = new App([
+    'settings' => $config['slim']
+]);
+
+// Slim container
+$container = $app->getContainer();
+
+// Slim view
+$container['view'] = function($c) {
+    $base_path = rtrim(str_ireplace('index.php', '', $c['request']->getUri()->getBasePath()), '/');
+
+    $view = new Twig(RESOURCE_ROOT.'/view', [
+        'charset'          => 'utf-8',
+        'cache'            => STORAGE_ROOT.'/cache/view',
+        'auto_reload'      => true,
+        'strict_variables' => false,
+        'autoescape'       => true
+    ]);
+
+    $view->addExtension(new TwigExtension($c['router'], $base_path));
+
+    return $view;
+};
 
 $app->get('/', function ($request, $response, $args) {
-    $response->write("Hello, It's work");
-
-    return $response;
-});
+    return $this->view->render($response, 'index.html');
+})->setName('index');
 
 $app->run();
