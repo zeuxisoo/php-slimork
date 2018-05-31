88bce56f226635091cabb8cecda2b725127bcdaa
Zeuxis Lo <seekstudio@gmail.com>
2018-05-31 17:37:23 +0800

    make sure the build-in helper functions was not exists

--- slimork/helpers.php
+++ slimork/helpers.php
@@ -6,30 +6,38 @@ if (function_exists('dd') === false) {
     }
 }
 
-function storage_path($path) {
-    return STORAGE_ROOT.$path;
+if (function_exists('storage_path') === false) {
+    function storage_path($path) {
+        return STORAGE_ROOT.$path;
+    }
 }
 
-function resources_path($path) {
-    return RESOURCES_ROOT.$path;
+if (function_exists('resources_path') === false) {
+    function resources_path($path) {
+        return RESOURCES_ROOT.$path;
+    }
 }
 
-function render_template($template_path, $template_name, $variables = []) {
-    $loader = new \Twig_Loader_Filesystem($template_path);
+if (function_exists('render_template') === false) {
+    function render_template($template_path, $template_name, $variables = []) {
+        $loader = new \Twig_Loader_Filesystem($template_path);
 
-    $twig = new \Twig_Environment($loader, [
-        'charset'          => 'utf-8',
-        'cache'            => false,
-        'auto_reload'      => true,
-        'strict_variables' => false,
-        'autoescape'       => 'html',
-    ]);
+        $twig = new \Twig_Environment($loader, [
+            'charset'          => 'utf-8',
+            'cache'            => false,
+            'auto_reload'      => true,
+            'strict_variables' => false,
+            'autoescape'       => 'html',
+        ]);
 
-    $template = $twig->load($template_name);
+        $template = $twig->load($template_name);
 
-    return $template->render($variables);
+        return $template->render($variables);
+    }
 }
 
-function render_error($template_name, $variables = []) {
-    return render_template(resources_path('/views/errors'), $template_name, $variables);
+if (function_exists('render_error') === false) {
+    function render_error($template_name, $variables = []) {
+        return render_template(resources_path('/views/errors'), $template_name, $variables);
+    }
 }
