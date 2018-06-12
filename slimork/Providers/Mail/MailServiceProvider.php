aca8915f36b17535641c5180f722ca3328c1c004
Zeuxis Lo <seekstudio@gmail.com>
2018-06-12 18:02:25 +0800

    improve the mail service provider comment

--- slimork/Providers/Mail/MailServiceProvider.php
+++ slimork/Providers/Mail/MailServiceProvider.php
@@ -9,7 +9,11 @@ use Slimork\Contracts\ServiceProvider;
  *
  * Mail:
  *
- *      $this->mail->subject('subject')->from('[emails]')->to('[emails]')->body('content')->send();
+ *      $this->mail->subject('subject')->from('email')->to('email')->body('content')->send();
+ *
+ *      $this->mail->subject('subject')->from('email')->to('email')->body('content')->attach('[file_paths]')->send();
+ *
+ *      $this->mail->subject('subject')->from('[emails]')->to('[emails]')->body('content')->attach('[file_paths]')->send();
  *
  */
 class MailServiceProvider extends ServiceProvider {
