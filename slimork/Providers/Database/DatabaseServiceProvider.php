f5f037eec52430d8af98640387072f4d6db37c92
Zeuxis Lo <seekstudio@gmail.com>
2018-07-16 13:30:57 +0800

    Make the database service provider can use independently not dependency on pagination service provider

--- slimork/Providers/Database/DatabaseServiceProvider.php
+++ slimork/Providers/Database/DatabaseServiceProvider.php
@@ -55,4 +55,39 @@ class DatabaseServiceProvider extends ServiceProvider {
         });
     }
 
+    public function boot() {
+        if ($this->container->has('paginator') === false) {
+            // Find current url and page no
+            $request     = $this->container->get('request');
+            $query_pairs = [];
+
+            parse_str($request->getUri()->getQuery(), $query_pairs);
+
+            if (array_key_exists('page', $query_pairs) === true) {
+                unset($query_pairs['page']);
+            }
+
+            $query_string = http_build_query($query_pairs);
+            $current_url  = (string) $request->getUri()->withQuery($query_string);
+            $current_page = $request->getParam('page');
+
+            // Setup paginator template
+            Paginator::defaultView('default.html');
+            Paginator::defaultSimpleView('default.simple.html');
+
+            // Setup paginator resolver
+            Paginator::viewFactoryResolver(function() {
+                return new PaginatorView($this->container);
+            });
+
+            Paginator::currentPathResolver(function () use ($current_url) {
+                return $current_url;
+            });
+
+            Paginator::currentPageResolver(function() use ($current_page) {
+                return $current_page;
+            });
+        }
+    }
+
 }
---
+++ slimork/Providers/Database/PaginatorView.php
@@ -0,0 +1,52 @@
+<?php
+namespace Slimork\Providers\Database;
+
+use Slimork\Contracts\PaginatorView as PaginatorViewContract;
+
+class PaginatorView implements PaginatorViewContract {
+
+    protected $view;
+    protected $template;
+    protected $variables;
+
+    public function __construct($container) {
+        $settings = $container->get('settings');
+
+        $loader = new \Twig_Loader_Filesystem(RESOURCES_ROOT.'/views/paginations');
+        $twig   = new \Twig_Environment($loader, [
+            'charset'          => 'utf-8',
+            'cache'            => false,
+            'auto_reload'      => true,
+            'strict_variables' => false,
+            'autoescape'       => 'html',
+        ]);
+
+        if (array_key_exists('view', $settings) === true) {
+            foreach($settings['view']['extensions'] as $extension) {
+                $twig->addExtension(new $extension($container));
+            }
+        }else{
+            $twig->addTest(new \Twig_SimpleTest('string', function($value) {
+                return is_string($value);
+            }));
+
+            $twig->addTest(new \Twig_SimpleTest('array', function($value) {
+                return is_array($value);
+            }));
+        }
+
+        $this->view = $twig;
+    }
+
+    public function make($view, $data = [], $merge_data = []) {
+        $this->template  = $this->view->load($view);
+        $this->variables = array_merge($data, $merge_data);
+
+        return $this;
+    }
+
+    public function render() {
+        return $this->template->render($this->variables);
+    }
+
+}
