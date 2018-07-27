0d4077b753893ff1303efd928345fd34a12bf71c
Zeuxis Lo <seekstudio@gmail.com>
2018-07-27 13:49:46 +0800

    add fetchOneRaw($sql) method into database service provider

--- slimork/Providers/Database/DatabaseManager.php
+++ slimork/Providers/Database/DatabaseManager.php
@@ -5,6 +5,15 @@ use Illuminate\Database\Capsule\Manager as CapsuleManager;
 
 class DatabaseManager extends CapsuleManager {
 
+    // Select in raw sql statement and only return first row like
+    public function fetchOneRaw($sql) {
+        $connection = $this->getConnection();
+
+        return collect($connection->select(
+            $connection->raw($sql)
+        ))->first();
+    }
+
     public function __call($method, $arguments) {
         return $this->getConnection()->$method(...$arguments);
     }
