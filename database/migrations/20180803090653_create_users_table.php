dbf2fb02d9c72f1f34f692cdf45967c80e2bd432
Zeuxis Lo <seekstudio@gmail.com>
2018-08-03 18:13:16 +0800

    fix the user migration file, the user field should be username

--- database/migrations/20180803090653_create_users_table.php
+++ database/migrations/20180803090653_create_users_table.php
@@ -9,7 +9,7 @@ class CreateUsersTable extends Migration {
     public function up() {
         Schema::create('users', function(Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
