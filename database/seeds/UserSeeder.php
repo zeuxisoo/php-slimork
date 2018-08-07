6cc0644313981e4c04821ae75a068a3dd703e5e4
Zeuxis Lo <seekstudio@gmail.com>
2018-08-07 14:24:55 +0800

    add default user seeder

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,14 @@
+<?php
+use Phinx\Seed\AbstractSeed;
+
+class UserSeeder extends AbstractSeed {
+
+    public function run() {
+        // \App\Models\User::create([
+        //     'username' => 'user',
+        //     'password' => \Hash::make('useruser'),
+        //     'email'    => 'test@test.com'
+        // ]);
+    }
+
+}
